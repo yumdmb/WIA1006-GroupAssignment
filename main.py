c40b0ca86fb03e4b931ef1b170e877ce928f3b0b
yumdmb <elvissawing.muran@gmail.com>
2023-06-10 21:28:17 +0800

    edit interface

--- main.py
+++ main.py
@@ -3,49 +3,70 @@ import streamlit as st
 import joblib
 
 def main():
+    # Set page config to wide layout and give it a title
+    st.set_page_config(layout="wide", page_title="Health Insurance Price Predictor")
+    
+    # Define custom CSS styles
+    css = """
+    <style>
+    body {
+        background-color: #1E1F20;
+        color: #FFFFFF;
+    }
+    .header {
+        background-color: #336699;
+        padding: 20px;
+        border-radius: 10px;
+        text-align: center;
+    }
+    .header h2 {
+        color: #FFFFFF;
+        font-family: Arial, sans-serif;
+    }
+
+    .success-message {
+        font-weight: bold;
+        font-size: 18px;
+        text-align: center;
+        margin-top: 20px;
+    }
+    </style>
+    """
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Health Insurance Price Predictor </h2>
     </div>
     """
+    # Apply custom CSS styles
+    st.markdown(css, unsafe_allow_html=True)
     
+    # Header section
     st.markdown(html_temp, unsafe_allow_html=True)
     
-    #model = joblib.load('model_joblib_rf')
+    # Model loading
+    # model = joblib.load('model_joblib_rf')
     model = joblib.load('model_joblib_dt')
     
-    age = st.slider('Enter Your Age', 18, 100)
-    s1 = st.selectbox('Sex', ('Male', 'Female'))
+    # Input section
+    st.sidebar.title("Your Details")
+    age = st.sidebar.slider('Age', 0, 100)
+    sex = st.sidebar.selectbox('Sex', ('Male', 'Female'))
+    bmi = st.sidebar.number_input('BMI', value=25.0, step=1.00)
+    children = st.sidebar.slider('Number of Children', 0, 10)
+    smoker = st.sidebar.selectbox('Smoker', ('No', 'Yes'))
+    region = st.sidebar.selectbox('Region', ('Northwest', 'Southwest', 'Southeast', 'Northeast'))
     
-    if s1 == 'Male':
-        sex = 1
-    else:
-        sex = 0
+    # Convert input values
+    sex = 1 if sex == 'Male' else 0
+    boolSmoker = 1 if smoker == 'Yes' else 0
+    region_mapping = {'Northwest': 0, 'Southwest': 1, 'Southeast': 2, 'Northeast': 3}
+    region_code = region_mapping[region]
     
-    bmi = st.number_input("Enter your BMI Value")
-    
-    children = st.slider('Enter the No. of Children', 0, 10)
-    smoker = st.selectbox('Smoker', ('yes', 'no'))
-    
-    if smoker == 'yes':
-        boolSmoker = 1
-    else:
-        boolSmoker = 0
-    
-    region_mapping = {
-        0: 'northwest',
-        1: 'southwest',
-        2: 'southeast',
-        3: 'northeast'
-    }
-    
-    region = st.slider('Enter your region', 0, 3)
-    region_description = region_mapping[region]
-    st.write(f"Selected region: {region_description}")
-    
-    if st.button('Predict'):
-        pred = model.predict([[age, sex, bmi, children, boolSmoker, region]])
-        st.success('Your Insurance Cost is {}'.format(round(pred[0], 2)))
+    # Make prediction
+    if st.sidebar.button('Predict'):
+        pred = model.predict([[age, sex, bmi, children, boolSmoker, region_code]])
+        st.markdown("<div class='prediction'><h3>Your Insurance Cost</h3><h2>${:.2f}</h2></div>".format(pred[0]), unsafe_allow_html=True)
+        st.markdown("<div class='success-message'>Thank you for using our service!</div>", unsafe_allow_html=True)
     
 if __name__ == '__main__':
     main()
